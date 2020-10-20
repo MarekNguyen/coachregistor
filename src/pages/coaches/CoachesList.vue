2b5e909f6e18ff31d480977c4761a3e6d4ea0507
Mark Nguyen <it@royalfashion.pl>
2020-10-20 16:59:43 +0200

    minor fix

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,5 +1,16 @@
 <template>
+<div>
     <div>
-        
+        Filter
     </div>
+    <div>
+        List of Coaches
+    </div>
+</div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
-import { createRouter,createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import CoachDetail from './pages/coaches/CoachDetail.vue'
 import CoachesList from './pages/coaches/CoachesList.vue'
 import CoachRegistration from './pages/coaches/CoachRegistration.vue'
 import ContactCoach from './pages/requests/ContactCoach'
-import RequestReceived from './pages/request/RequestReceived.vue'
+import RequestReceived from './pages/requests/RequestReceived.vue'
 import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path:'/', redirect: '/coaches'},
+        { path:'/', redirect: '/coaches'},
         { path: '/coaches', component: CoachesList },
         { path: '/coaches/:id', component: CoachDetail, children: [
             {path: '/contact', component: ContactCoach}
@@ -19,3 +19,5 @@ const router = createRouter({
         { path: '/:notFound(.*)', component: NotFound},
     ]
 })
+
+export default router;
