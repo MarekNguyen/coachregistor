dbd159ea23c0631895d3d8ed33dd86de212a214d
Mark Nguyen <it@royalfashion.pl>
2020-10-21 21:50:44 +0200

    minor fix

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import { createApp } from 'vue';
 import router from './router'
 import App from './App.vue'
 import store from './store/index'
+
+import BaseCard from './components/ui/BaseCard.vue'
+import BaseButton from './components/ui/BaseButton.vue'
+import BaseBadge from './components/ui/BaseButton.vue'
+
 const app = createApp(App)
 app.use(router);
 app.use(store);
-
+app.component('app-card', BaseCard)
+app.component('app-button', BaseButton)
+app.component('app-badge', BaseBadge)
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,25 +1,29 @@
 <template>
-<div>
+<section>
     <div>
         Filter
     </div>
-    <div>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{ coach.firstName }}
-            </li>
-        </ul>
-        <h3 v-else>No Coaches Found</h3>
-    </div>
-</div>
+    <section>
+        <app-card>
+            <div class="controls">
+                <app-button mode="outline">Refresh</app-button>
+                <app-button link :to="'/register'">Register as Coach</app-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :firstName="coach.firstName" :lastName="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+            </ul>
+            <h3 v-else>No Coaches Found</h3>
+        </app-card>
+    </section>
+</section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue'
 export default {
+    components: {
+        CoachItem
+    },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches']
@@ -30,3 +34,16 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.controls {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
